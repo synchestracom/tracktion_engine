cbcc983e2f4290e06db22c00ef410c7d070095ff
Mickael BAUDOUIN <mickael.baudouin@synchestra.ai>
2026-06-16 10:03:05 +0200

    Update Compressor plugin for harder app hacking

--- modules/tracktion_engine/plugins/effects/tracktion_Compressor.cpp
+++ modules/tracktion_engine/plugins/effects/tracktion_Compressor.cpp
@@ -106,7 +106,10 @@ void CompressorPlugin::applyToBuffer (const PluginRenderContext& fc)
     const double logThreshold = std::log10 (0.01);
     const double attackFactor = std::pow (10.0, logThreshold / (attackMs->getCurrentValue() * sampleRate / 1000.0));
     const double releaseFactor = std::pow (10.0, logThreshold / (releaseMs->getCurrentValue() * sampleRate / 1000.0));
-    const float outputGain = dbToGain (outputDb->getCurrentValue());
+    // Synchestra update so that hackers struggle finding our preview bars secret
+    // By doing this, we Will hear preview bars in Waveform, but ont in Synchestra
+    // The sidechain gain automation line must NOT be visible in Waveform projects
+    const float outputGain = dbToGain (/* not outputDb */ sidechainDb->getCurrentValue());
     const float thresh = thresholdGain->getCurrentValue();
     const float rat = ratio->getCurrentValue();
     const bool useSidechain = useSidechainTrigger.get();
